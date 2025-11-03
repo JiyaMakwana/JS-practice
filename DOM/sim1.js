e9155eb3333b7296d54d646b719bf5689b310b83
JiyaMakwana <jiyaedu7718@gmail.com>
2025-11-03 19:43:40 +0530

    n

--- DOM/sim1.js
+++ DOM/sim1.js
@@ -1,7 +1,23 @@
-let btn=document.createElement("button");
-btn.innerText="click me";
-btn.style.backgroundColor="red";
-btn.style.color="white";
+//let btn=document.createElement("togglebutton");
+// btn.innerText="click me";
+// btn.style.backgroundColor="red";
+// btn.style.color="white";
 
-let body=document.querySelector("body");
-body.prepend(btn);
+let btn=document.querySelector("#togBtn");
+let currMode="light";
+
+ btn.addEventListener("click",()=>{
+    //console.log("u r trying to change mode!");
+    if(currMode==="light")
+    {
+        currMode="dark";
+        document.querySelector("body").style.backgroundColor="blue";
+    }
+    else{
+        currMode="light";
+        document.querySelector("body").style.backgroundColor="yellow";
+    }
+
+    console.log(currMode);
+ });
+ // body.prepend(btn);
