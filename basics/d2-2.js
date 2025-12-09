2dff33fdd4223d9949ed9423d5f67e4b15a6c959
JiyaMakwana <jiyaedu7718@gmail.com>
2025-12-09 11:53:42 +0530

    c

---
+++ basics/d2-2.js
@@ -0,0 +1,12 @@
+// Check if a number is in range (10–20).
+function range(num){
+    return num>=10 && num<=20;
+}
+console.log(range(21));
+console.log(typeof null);
+console.log(typeof undefined);
+console.log("5" * 2);
+console.log("5" + 2);
+console.log("5" - "2");
+console.log(Boolean("0"));
+console.log(Boolean(0));
