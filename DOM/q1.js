7f6c264afaccab168d404badc3ea319b99a140cd
JiyaMakwana <jiyaedu7718@gmail.com>
2025-12-14 21:40:04 +0530

    c

--- DOM/q1.js
+++ DOM/q1.js
@@ -9,3 +9,5 @@ const inner1=document.getElementsByClassName("inner1")[0];
 inner1.innerHTML="Welcome to Javascript DOM";
 const inner2=document.getElementById("inner2");
 inner2.innerHTML="Hello <strong>Javascript</strong> DOM";
+
+const inner3=document.getElementById("inner3").innerHTML=`<p> New Head</p> <h2>I am from inner 3</h2>`;
